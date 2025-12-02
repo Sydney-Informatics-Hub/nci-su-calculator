3d5b4e7c89fc8c45d29bd90069cb3936a2aa7aaf
georgiesamaha <georgiesamaha@gmail.com>
2025-12-02 19:49:21 +1100

    Scorched earth do over

---
+++ queues.js
@@ -0,0 +1,20 @@
+const QUEUE_PARAMS = {
+  normal: { rate: 2.0, cores: 48, mem: 192, walltime: 48 },
+  express: { rate: 6.0, cores: 48, mem: 192, walltime: 24, },
+  hugemem: { rate: 3.0, cores: 48, mem: 3072, walltime: 48 },
+  megamem: { rate: 5.0, cores: 48, mem: 6144, walltime: 48 },
+
+  normalbw: { rate: 1.25, cores: 28, mem: 128, walltime: 48 },
+  expressbw: { rate: 3.75, cores: 28, mem: 128, walltime: 24 },
+  hugemembw: { rate: 1.25, cores: 28, mem: 1020, walltime: 48 },
+  megamembw: { rate: 1.25, cores: 64, mem: 3000, walltime: 48 },
+
+  normalsl: { rate: 1.5, cores: 36, mem: 192, walltime: 48 },
+  normalsr: { rate: 2.0, cores: 104, mem: 192, walltime: 48 },
+  expresssr: { rate: 6.0, cores: 104, mem: 192, walltime: 24 },
+
+  gpuvolta: { rate: 3.0, cores: 48, mem: 384, walltime: 48, gpus: 4, gpuram: 160},
+  dgxa100: { rate: 4.5, cores: 128, mem: 2000, walltime: 48, gpus: 8, gpuram:  640},
+
+  copyq: { rate: 2.0, cores: 1, mem: 190, walltime: 10}
+};
