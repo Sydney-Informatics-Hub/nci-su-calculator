32622c9929e89a3322874f6fdf5112e14f75ceef
Matthew Downton <4353654+mattdton@users.noreply.github.com>
2025-12-05 14:50:58 +1100

    Update queues.js

--- queues.js
+++ queues.js
@@ -9,11 +9,11 @@ const QUEUE_PARAMS = {
   hugemembw: { rate: 1.25, cores: 28, mem: 1020, disk: 390, walltime: 48, core_fp64: 0.05 },
   megamembw: { rate: 1.25, cores: 64, mem: 3000, disk: 800, walltime: 48, core_fp64: 0.05 },
 
-  normalsl: { rate: 1.5, cores: 36, mem: 192, disk: 400, walltime: 48, core_fp64: 0.05 },
-  normalsr: { rate: 2.0, cores: 104, mem: 192, disk: 400, walltime: 48, core_fp64: 0.05 },
-  expresssr: { rate: 6.0, cores: 104, mem: 192, disk: 400, walltime: 24, core_fp64: 0.05 },
+  normalsl: { rate: 1.5, cores: 32, mem: 192, disk: 400, walltime: 48, core_fp64: 0.05 },
+  normalsr: { rate: 2.0, cores: 104, mem: 512, disk: 400, walltime: 48, core_fp64: 0.05 },
+  expresssr: { rate: 6.0, cores: 104, mem: 512, disk: 400, walltime: 24, core_fp64: 0.05 },
 
-  gpuvolta: { rate: 3.0, cores: 48, mem: 384, disk: 400, walltime: 48, gpus: 4, gpuram: 160, core_fp64: 0.05, gpu_fp64: 7 },
+  gpuvolta: { rate: 3.0, cores: 48, mem: 384, disk: 400, walltime: 48, gpus: 4, gpuram: 128, core_fp64: 0.05, gpu_fp64: 7 },
   dgxa100: { rate: 4.5, cores: 128, mem: 2000, disk: 28000, walltime: 48, gpus: 8, gpuram:  640, core_fp64: 0.05, gpu_fp64: 9.7 },
   gpuhopper: { rate: 7.5, cores: 48, mem: 1000, disk: 1700, walltime: 48, gpus: 4, gpuram: 564, core_fp64: 0.05, gpu_fp64: 34 },
 
